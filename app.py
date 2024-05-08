7ae73218fcf4ce433c2b4bf30b536e072becd637
Simone <ssimone12@gmail.com>
2024-05-08 16:23:28 +0200

    git init

---
+++ app.py
@@ -0,0 +1,29 @@
+# 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
+
+# https://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@github.com/simonerok/mysite.git
+
+#########################
+from bottle import default_app, get, post, run, template, static_file
+import git
+ 
+@post('/secret_url_for_git_hook')
+def git_update():
+  repo = git.Repo('./mysite')
+  origin = repo.remotes.origin
+  repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+  origin.pull()
+  return ""
+ 
+ 
+##############################
+@get("/")
+def _():
+    return "One"
+ 
+##############################
+try:
+  import production
+  application = default_app()
+except Exception as ex:
+  print("Running local server")
+  run(host="127.0.0.1", port=80, debug=True, reloader=True) 
